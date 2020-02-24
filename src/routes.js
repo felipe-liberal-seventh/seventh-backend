7d5afa1fe7d9f84f0e8e710a109514b1faf88c2e
todd7 <edson_a@outlook.com>
2020-02-23 21:46:47 -0300

    add cors

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import MonitoringController from './app/controllers/MonitoringController';
+import cors from './app/middlewares/cors';
 
 const routes = new Router();
 
+routes.use(cors);
 routes.post('/monitoring', MonitoringController.store);
-// routes.get('/monitoring', MonitoringController.index);
-routes.get('/monitoring', MonitoringController.update);
+routes.get('/monitoring', MonitoringController.index);
+// routes.get('/monitoring', MonitoringController.update);
 
 export default routes;
